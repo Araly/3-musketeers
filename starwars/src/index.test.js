cad1ccbaf41ab9eee08b8967338cfd7da76cbb77
araly <araly.isaac@protonmail.com>
2019-03-04 11:51:22 +0100

    test: completed 'all'

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -6,25 +6,48 @@ describe('starwars-names', () => {
         test('should be a fulfilled array', () => {
             expect(starWars.all).toHaveLength(starWarsNames.length)
         });
+        expect.extend({
+            toBeArrayContainingStrings(received) {
+                var result = true;
+                for (element in received) {
+                    if (typeof element != "string") {
+                        result = false;
+                    }
+                }
+                if (result) {
+                    return {
+                        message: () =>
+                            `expected ${received} not to be an array containing strings`,
+                        pass: true,
+                    };
+                }
+                else {
+                    return {
+                        message: () =>
+                            `expected ${received} to be an array containing strings`,
+                        pass: false,
+                    };
+                }
+            },
+        });
         test('should be an array of strings', () => {
-            expect(starWars.all).
-            throw new Error('test not yet defined... remove the throw and write your test here');
+            expect(starWars.all).toBeArrayContainingStrings()
         });
-        
+
         test('should contain `Luke Skywalker`', () => {
-            throw new Error('test not yet defined... remove the throw and write your test here');
+            expect(starWars.all).toContain("Luke Skywalker")
         });
-        
+
         test('should not contain `Ben Quadinaros`', () => {
-            throw new Error('test not yet defined... remove the throw and write your test here');
+            expect(starWars.all).not.toContain("Ben Quadinaros")
         });
     });
-    
+
     describe('random', () => {
         test('should return a random item from the starWars.all', () => {
             throw new Error('test not yet defined... remove the throw and write your test here');
         });
-        
+
         test('should return an array of random items if passed a number', () => {
             throw new Error('test not yet defined... remove the throw and write your test here');
         });
